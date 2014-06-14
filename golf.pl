a6512f396c4778965280d216c6dcf9e89952232d
cono <q@cono.org.ua>
2014-06-14 04:05:30 +0300

    % wc -c golf.pl 222 golf.pl

--- golf.pl
+++ golf.pl
@@ -1,2 +1,2 @@
 #!/usr/bin/perl -alpF:
-INIT{$h{Erdos}={_=>1}}sub z{$h{$i=$_[0]}{_}?0:do{my@k=keys%{$h{$i}};local$h{$i};@g=sort{$a-$b}map{z($_)}@k;(@g,"inf")[0]+1}}$n=do{@n=split/, /,$F[0];@{$h{$_}}{@n}=(1)x@n for@n;$n}||$F[0]}{$_=ucfirst z$n
+INIT{$h{Erdos}={_=>1}}sub z{$h{$i=$_[0]}{_}?0:do{my@k=keys%{$h{$i}};local$h{$i};((sort{$a-$b}map{z($_)}@k),"inf")[0]+1}}$n=do{@n=split/, /,$F[0];@{$h{$_}}{@n}=(1)x@n for@n;$n}||$F[0]}{$_=ucfirst z$n
